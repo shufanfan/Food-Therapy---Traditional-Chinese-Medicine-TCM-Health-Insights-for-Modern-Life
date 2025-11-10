d13e81c353f50b07196bb3438d3f8d80183a9525
huiqinhu5500 <hhu01@wesleyan.edu>
2025-11-09 22:29:26 -0800

    Add About TCM Constitution Theory page with 9 constitution types explanation (Sprint 2 Item 7)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import LandingPage from "./components/LandingPage";
 import Survey from "./components/Survey";
 import SurveyResults from "./components/SurveyResults";
 import PrivacyPolicy from "./components/PrivacyPolicy";
+import AboutTCM from "./components/AboutTCM";
 
 function AppContent() {
   const [surveyAnswers, setSurveyAnswers] = useState<string[]>([]);
@@ -25,6 +26,7 @@ function AppContent() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/privacy" element={<PrivacyPolicy />} />
+      <Route path="/about-tcm" element={<AboutTCM />} />
       <Route
         path="/assessment"
         element={<Survey onComplete={handleSurveyComplete} />}
